24537238ec802c0ad64027e35e0b3f55da32f6f6
Keebler17 <keebler17.exe@gmail.com>
2020-02-08 20:44:55 -0500

    it actually works LOL

--- source/main.c
+++ source/main.c
@@ -14,11 +14,21 @@ void fillBuffer(gfxScreen_t screen, u8* buffer, u8 red, u8 green, u8 blue);
 void clearBuffer(gfxScreen_t screen, u8* buffer);
 void drawImage(gfxScreen_t screen, int _x, int _y, int height, int width, int image[], u8* buffer);
 int getXYPos(int x, int y, gfxScreen_t screen);
+int getWinner(int p1, int p2);
 
 int main(int argc, char* argv[])
 {
 	
-	int choice;
+	int choice = -1;
+	bool chose = false;
+	
+	int turn = 0;
+	
+	int p1 = -1;
+	int p2 = -1;
+	
+	int p1_score = 0;
+	int p2_score = 0;
 	
 	gfxInit(GSP_BGR8_OES, GSP_BGR8_OES, false);
 	
@@ -27,41 +37,93 @@ int main(int argc, char* argv[])
 	consoleInit(GFX_TOP, &console);
 	consoleSelect(&console);
 	
+	bool tie = false;
+	bool p1_win = false;
+	bool p2_win = false;
+	
 	while (aptMainLoop())
 	{
+		// fillBuffer(GFX_BOTTOM, gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, 0, 0), 0x00, 0x00, 0x00);
+		drawImage(GFX_BOTTOM, 0, 0, BOTTOM_HEIGHT, BOTTOM_WIDTH, menu, gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, 0, 0));
+		consoleClear();
+		
+		if(tie) {
+			printf("\x1b[0;0Htie");
+		} else if(p1_win) {
+			printf("\x1b[0;0Hp1");
+		} else if(p2_win) {
+			printf("\x1b[0;0Hp2");
+		}
+		
+		printf("\x1b[2;0H%d - %d", p1_score, p2_score);
+		
 		touchPosition touch;
-		
 		hidScanInput();
-		
 		hidTouchRead(&touch);
-		
 		u32 kDown = hidKeysDown();
 		
-		drawImage(GFX_BOTTOM, 0, 0, BOTTOM_HEIGHT, BOTTOM_WIDTH, menu, gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, 0, 0));
+		if (kDown & KEY_START) { 
+			break;
+		}
 		
 		
-		consoleClear();
-		printf("\x1b[1;0HTouch X: %d", touch.px);
-		printf("\x1b[2;1HTouch Pad Y: %d", touch.py);
+		if(turn == 0) {
+			p1 = choice;
+		} else {
+			p2 = choice;
+			
+			int result = getWinner(p1, p2);
+			
+			tie = false;
+			p1_win = false;
+			p2_win = false;
+			
+			if(result == -1) {
+				tie = true;
+			} else if(result == 0) {
+				p1_win = true;
+			} else {
+				p2_win = true;
+			}
+			
+		}
 		
-		choice = -1;
+		if(chose && !tie) {
+			if(p1_win) {
+				p1_score++;
+			}
+			
+			if(p2_win) {
+				p2_score++;
+			}
+		}
 		
-		while(!chose) {
-			if (kDown & KEY_START) 
-					break;
-				
-			if(touch.px >= 35 && touch.px <= 284) {
-				if(touch.py >= 50 && touch.py < 100) { // rock
-					choice = 0
+		chose = false;
+		if(touch.px >= 35 && touch.px <= 284 && kDown & KEY_TOUCH) {
+			if(touch.py >= 50 && touch.py < 100) { // rock
+				choice = 0;
+				if(turn == 0) {
+					turn = 1;
+				} else {
+					turn = 0;
 				}
-				
-				if(touch.py >= 100 && touch.py < 149) { // paper 
-					choice = 1
+				chose = true;
+			} else if(touch.py >= 100 && touch.py < 149) { // paper 
+				choice = 1;
+				if(turn == 0) {
+					turn = 1;
+				} else {
+					turn = 0;
 				}
-				
-				if(touch.py >= 150 && touch.py <= 200) { // scissors
-					choice = 2
+				chose = true;
+			} else if(touch.py >= 150 && touch.py <= 200) { // scissors
+				choice = 2;
+				if(turn == 0) {
+					turn = 1;
+				} else {
+					turn = 0;
 				}
+				chose = true;
 			}
 		}
 		
@@ -70,7 +132,6 @@ int main(int argc, char* argv[])
 		
 		gspWaitForVBlank();
 	}
-
 	gfxExit();
 	return 0;
 }
@@ -151,4 +212,33 @@ void drawImage(gfxScreen_t screen, int _x, int _y, int height, int width, int im
 int getXYPos(int x, int y, gfxScreen_t screen) {
 	int height = screen == GFX_TOP ? TOP_HEIGHT : BOTTOM_HEIGHT;
 	return (x * 3 * height) + (y * 3);
+}
+
+int getWinner(int p1, int p2) {
+	if (p1 == 0) {
+		if(p2 == 0) {
+			return -1;
+		} else if (p2 == 1) {
+			return 1;
+		} else if (p2 == 2) {
+			return 0;
+		}
+	} else if (p1 == 1) {
+		if (p2 == 0) {
+			return 0;
+		} else if(p2 == 1) {
+			return -1;
+		} else if (p2 == 2) {
+			return 1;
+		}
+	} else if (p1 == 2) {
+		if (p2 == 0) {
+			return 1;
+		} else if (p2 == 1) {
+			return 0;
+		} else if(p2 == 2) {
+			return -1;
+		}
+	}
+	return 0xFF;
 }
